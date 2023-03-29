52089ff74339ba970d7e6265ebab5bf122ce1d8b
Raniel Dourado <rnldourado@gmail.com>
2023-03-29 11:49:06 -0300

    aula-29-03-2023

---
+++ mercadofacil/src/main/java/com/ufcg/psoft/mercadofacil/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package com.ufcg.psoft.mercadofacil.repository;
+
+public interface ProdutoRepository <Produto, Long>{
+    Produto update(Produto produtoAlterado);
+    Produto find(Long id);
+
+
+}
---
+++ mercadofacil/src/test/java/com/ufcg/psoft/mercadofacil/service/ProdutoAlterarServiceTest.java
@@ -0,0 +1,84 @@
+package com.ufcg.psoft.mercadofacil.service;
+
+import com.ufcg.psoft.mercadofacil.model.Produto;
+import com.ufcg.psoft.mercadofacil.repository.ProdutoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Testes para alteração do Produto")
+public class ProdutoAlterarServiceTest {
+
+    @Autowired
+    ProdutoAlterarService driver;
+
+    @MockBean
+    ProdutoRepository<Produto, Long> produtoRepository;
+    Produto produto;
+
+    @BeforeEach
+    void setup (){
+        Mockito.when(produtoRepository.find(10L))
+                .thenReturn(Produto.builder()
+                .id(10L)
+                .codigoBarra("7899137500104")
+                .nome("Produto dez")
+                .fabricante("Empresa dez")
+                .preco(450.00)
+                .build()
+                );
+
+        produto = produtoRepository.find(10L);
+
+        Mockito.when(produtoRepository.update(produto))
+                .thenReturn(Produto.builder()
+                        .id(10L)
+                        .codigoBarra("7899137500104")
+                        .nome("Nome Produto Alterado")
+                        .fabricante("Nome Fabricante Alterado")
+                        .preco(500.0)
+                        .build()
+                );
+
+    }
+
+    @Test
+    @DisplayName("Quando altero o nome do produto com um nome válido")
+    void alterarNomeDoProduto() {
+        /* AAA */
+        //Arrange
+        produto.setNome("Nome Produto Alterado");
+        //Act
+        Produto resultado = driver.alterar(produto);
+        //Assert
+        assertEquals("Nome Produto Alterado", resultado.getNome());
+    }
+
+    @Test
+    @DisplayName("Quando o preço é menor ou igual a zero")
+    void precoMenorIgualAZero() {
+        //Arrange
+        produto.setPreco(0.0);
+        //Act
+        Produto resultado = driver.alterar(produto);
+        //Assert
+        RuntimeException thrown = assertThrows(
+                RuntimeException.class,
+                () -> driver.alterar(produto)
+        );
+        assertEquals("Preco invalido", thrown.getMessage());
+
+    }
+
+    //todo implementar servico do codigo de barras e passando nome null na alteração
+}
